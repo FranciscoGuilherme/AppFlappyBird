0dc6077284ca5c2a70506a32b058595f58da82ba
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2018-08-10 09:35:08 -0300

    FGB | Animando o pássaro - Bater asas - Cair - Evento touch

--- core/src/com/projeto/flappybird/FlappyBird.java
+++ core/src/com/projeto/flappybird/FlappyBird.java
@@ -6,28 +6,61 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class FlappyBird extends ApplicationAdapter
+{
+
+    private SpriteBatch batch;
+    private Texture[] bird;
+    private Texture background_day;
+
+    private int altura;
+    private int largura;
+
+    private float variacao = 0;
+    private float velocidadeQueda = 0;
+    private float posicaoInicialVertical = 0;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create ()
+    {
+        batch = new SpriteBatch();
+        bird = new Texture[3];
+
+        bird[0] = new Texture( "bird_up.png" );
+        bird[1] = new Texture( "bird_middle.png" );
+        bird[2] = new Texture( "bird_down.png" );
+
+        background_day = new Texture( "day.png" );
+
+        altura = Gdx.graphics.getWidth();
+        largura = Gdx.graphics.getHeight();
+        posicaoInicialVertical = altura / 2;
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render ()
+    {
+        variacao += Gdx.graphics.getDeltaTime() * 15;
+        velocidadeQueda++;
+
+        if ( variacao > 3 ) variacao = 0;
+
+        if ( Gdx.input.justTouched() )
+        {
+            velocidadeQueda = -15;
+        }
+
+        if ( posicaoInicialVertical > 0 || velocidadeQueda < 0 )
+        {
+            posicaoInicialVertical -= velocidadeQueda;
+        }
+
+        batch.begin();
+
+        batch.draw( background_day, 0, 0, altura, largura );
+        batch.draw( bird[ (int) variacao ], 30, posicaoInicialVertical );
+
+        batch.end();
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
